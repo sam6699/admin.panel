7d9b36b639fce73b65a23f9fc477060fe716235c
bob153 <morgan95@inbox.ru>
2019-09-16 13:48:54 +0300

    Created_Reg_Auth

--- app/SBlog/Core/Registry.php
+++ app/SBlog/Core/Registry.php
@@ -11,5 +11,27 @@ namespace App\SBlog\Core;
 
 class Registry
 {
+    use TSingleton;
+    protected static $properties=[];
+
+    public function setProperty($name,$value){
+        self::$properties[$name]=$value;
+
+    }
+
+    public function getProperty($name){
+
+        if (isset(self::$properties[$name]))
+            return self::$properties[$name];
+
+        return null;
+    }
+
+    public function getAllProperties(){
+
+        return self::$properties;
+
+    }
+
 
 }
